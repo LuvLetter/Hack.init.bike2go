bdd93b8ddba4b477e7611bc9817f209419801b66
jhl <jhl@aAA.local>
2017-07-08 19:37:56 +0800

    starting

--- server/server.py
+++ server/server.py
@@ -1,10 +1,60 @@
-"""First hug API (local and HTTP access)"""
-import hug
+
+#!/usr/bin/env python
+ 
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+def isLegalRequest(bikeId, bikeStatus):
+  if(0==1):                                       
+    return False
+  return True
+
+def updateLocation(bikeId,bikeStatus):
+  return
+
+def updateStatus(bikeId, bikeStatus):
+  return
+
+def update(bikeId, bikeStatus):
+  updateLocation(bikeId,bikeStatus)
+  updateStatus(bikeId, bikeStatus)
+  return 
 
 
-@hug.get(examples='name=Timothy&age=26')
-@hug.local()
-def happy_birthday(name: hug.types.text, age: hug.types.number, hug_timer=3):
-    """Says happy birthday to a user"""
-    return {'message': 'Happy {0} Birthday {1}!'.format(age, name),
-            'took': float(hug_timer)}
+def getPassword(bikeId,bikeStatus):
+  if isLegalRequest(bikeId, bikeStatus):
+    return "123"
+  return
+
+# HTTPRequestHandler class
+class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+ 
+  # GET
+  def do_GET(self):
+        # Send response status code
+        self.send_response(200)
+ 
+        # Send headers
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+ 
+        # Send message back to client
+        message = "Hello world!"
+        # Write content as utf-8 data
+        self.wfile.write(bytes(message, "utf8"))
+        return
+ 
+def run():
+  print('starting server...')
+ 
+  # Server settings
+  # Choose port 8080, for port 80, which is normally used for a http server, you need root access
+  server_address = ('127.0.0.1', 3306)
+  httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
+  print('running server...')
+  httpd.serve_forever()
+ 
+ 
+run()
+
+
+
