cceadf6de8db27908a4c87306ace07915de56153
luvletter <i@luvletter.moe>
2017-07-09 06:42:30 +0800

    update

--- server/server.py
+++ server/server.py
@@ -10,7 +10,7 @@ import sqlite3 as lite
 
 
 def isLegalRequest(bike):
-  if(decrypt(bike)!=bike.bikeId):                                       
+  if(decrypt(bike)!=bike.bikeId):
     return False
   return True
 
@@ -28,7 +28,7 @@ def updateStatus(bike, status):
 def update(bike, bikeStatus):
   updateLocation(bike,lng,lat)
   updateStatus(bike, bikeStatus)
-  return 
+  return
 
 
 def getPassword(bikeId,bikeStatus):
@@ -86,7 +86,7 @@ def check(bike):
   return (result%10000) == bike.encrypted
 
 def isLegalRequest(bike):
-  if(decrypt(bike)!=bike.bikeId):                                       
+  if(decrypt(bike)!=bike.bikeId):
     return False
   return True
 
@@ -104,35 +104,31 @@ def getPassword(bike):
 
 # HTTPRequestHandler class
 class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
- 
   # GET
   def do_GET(self):
 
         # Send response status code
         self.send_response(200)
- 
+
         # Send headers
         self.send_header('Content-type','text/html')
         self.end_headers()
- 
+        parsed_path = urlparse.urlparse(self.path)
         # Send message back to client
         message = "Hello world!"
         # Write content as utf-8 data
         self.wfile.write(bytes(message, "utf8"))
         return
- 
+
 def run():
   print('starting server...')
- 
+
   # Server settings
   # Choose port 8080, for port 80, which is normally used for a http server, you need root access
   server_address = ('127.0.0.1', 3306)
   httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
   print('running server...')
   httpd.serve_forever()
- 
- 
+
+
 # run()
-
-
-
