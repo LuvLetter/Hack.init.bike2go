5fa4da1e17b6311683155e5c7f3dc07cf32ce0aa
jhl <jhl@aAA.local>
2017-07-08 17:00:28 +0800

    try

--- server/server.py
+++ server/server.py
@@ -1,5 +1,10 @@
-import http
-def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
-    server_address = ('', 80)
-    httpd = server_class(server_address, handler_class)
-    httpd.serve_forever()
+"""First hug API (local and HTTP access)"""
+import hug
+
+
+@hug.get(examples='name=Timothy&age=26')
+@hug.local()
+def happy_birthday(name: hug.types.text, age: hug.types.number, hug_timer=3):
+    """Says happy birthday to a user"""
+    return {'message': 'Happy {0} Birthday {1}!'.format(age, name),
+            'took': float(hug_timer)}
